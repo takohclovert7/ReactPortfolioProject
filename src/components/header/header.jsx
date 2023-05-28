798cc0c51344339e7040121578d50dea718bf377
takohclovert7 <takohclovert7@gmail.com>
2023-05-28 17:58:53 +0100

    comp added about_experience_testimony_portfolio_start-narbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,7 +10,7 @@ return (
 <div className='container header__container'>
 <h5>Hello I'm</h5>
 <h1>Takoh Clovert Nfua</h1>
-<h5 className='text-light'>Network Engineer </h5>
+<h5 className='text-light'>Network Engineer </h5>   
 <CTA/>
 <HeaderSocial/>
 <div className='my_image'>
